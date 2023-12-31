1f056aacc0899753ccfd0036b851c9a2ec9036c5
Qingguo Zhang <qingguozhang2000@gmail.com>
2023-12-31 00:00:35 -0500

    add mutext/semaphore

--- guest-wait.c
+++ guest-wait.c
@@ -3,8 +3,13 @@
 struct GuestQueue guest_queue;
 int last_guest_id = 0;
 
-pthread_t guest_th;
-pthread_mutex_t mutex_guest_queue;
+pthread_t guest_arrival_thread;
+pthread_t guest_seating_thread;
+
+pthread_mutex_t guest_queue_mutex;
+
+sem_t *q_empty_sem;
+sem_t *q_full_sem;
 
 void on_start_guest_wait()
 {
@@ -13,23 +18,39 @@ void on_start_guest_wait()
     guest_queue.p_queue_head = NULL;
     guest_queue.p_queue_tail = NULL;
 
-    assert(pthread_create(&guest_th, NULL, guest_thread_worker, NULL) == 0);
-    assert(pthread_mutex_init(&mutex_guest_queue, NULL) == 0);
+    assert(pthread_create(&guest_arrival_thread, NULL, guest_arrival_worker, NULL) == 0);
+    assert(pthread_create(&guest_seating_thread, NULL, guest_seating_worker, NULL) == 0);
+
+    assert(pthread_mutex_init(&guest_queue_mutex, NULL) == 0);
+
+    sem_unlink("/empty_sem");
+    q_empty_sem = sem_open("/empty_sem", O_CREAT | O_EXCL, 0644, WAIT_QUEUE_LIMIT);
+    assert(q_empty_sem != SEM_FAILED);
+
+
+    sem_unlink("/full_sem");
+    q_full_sem = sem_open("/full_sem", O_CREAT | O_EXCL, 0644, 0);
+    assert(q_full_sem != SEM_FAILED);
 }
 
 void on_finish_guest_wait()
 {
-    assert(pthread_mutex_destroy(&mutex_guest_queue) == 0);
-    assert(pthread_join(guest_th, NULL) == 0);
-
     print_queue();
+
+    assert(pthread_join(guest_arrival_thread, NULL) == 0);
+    assert(pthread_join(guest_seating_thread, NULL) == 0);
+
+    assert(pthread_mutex_destroy(&guest_queue_mutex) == 0);
+
+    assert(sem_close(q_empty_sem) == 0);
+    assert(sem_close(q_full_sem) == 0);
+
+    sem_unlink("/empty_sem");
+    sem_unlink("/full_sem");
 }
 
-void on_guest_arriving(char *party_name, int party_size)
+void add_guest_to_queue(char *party_name, int party_size)
 {
-    printf("Guest arrived: %s - %d people\n", party_name, party_size);
-    pthread_mutex_lock(&mutex_guest_queue);
-
     last_guest_id++;
 
     struct Guest *p_guest = malloc(sizeof(struct Guest));
@@ -51,13 +72,11 @@ void on_guest_arriving(char *party_name, int party_size)
         guest_queue.p_queue_tail->p_next = p_guest;
         guest_queue.p_queue_tail = p_guest;
     }
-
-    pthread_mutex_unlock(&mutex_guest_queue);
 }
 
-void *guest_thread_worker()
+void *guest_arrival_worker()
 {
-    for (int i = 0; i < 10; i++)
+    while (1)
     {
         int sleep_time = rand() % 1000000;
         usleep(sleep_time);
@@ -66,9 +85,50 @@ void *guest_thread_worker()
         char *name = names[j];
         int party_size = rand() % max_party_size + 1;
 
-        on_guest_arriving(name, party_size);
+        printf("Guest arrived: %s - %d people\n", name, party_size);
+        printf("Waiting for adding to queue\n");
+
+        sem_wait(q_empty_sem);
+        pthread_mutex_lock(&guest_queue_mutex);
+
+        add_guest_to_queue(name, party_size);
+
+        printf("Added the guest party %d (%s) to queue\n", last_guest_id, name);
+
+        pthread_mutex_unlock(&guest_queue_mutex);
+        sem_post(q_full_sem);
     }
 
+    return NULL;
+}
+
+void *guest_seating_worker()
+{
+    while (1) {
+        printf("Waiting for queue populated\n");
+
+        sem_wait(q_full_sem);
+        pthread_mutex_lock(&guest_queue_mutex);
+
+        print_queue();
+
+        struct Guest *p_head = guest_queue.p_queue_head;
+
+        printf("Removing %d - %s from waiting queue\n", p_head->id, p_head->name);
+
+        guest_queue.p_queue_head = p_head->p_next;
+        if (p_head->p_next != NULL) {
+            p_head->p_next->p_prev = NULL;
+        } else {
+            guest_queue.p_queue_tail = NULL;
+        }
+        free(p_head);
+
+        print_queue();
+
+        pthread_mutex_unlock(&guest_queue_mutex);
+        sem_post(q_empty_sem);
+    }
     return NULL;
 }
 
--- guest-wait.h
+++ guest-wait.h
@@ -3,7 +3,7 @@
 #ifndef GUEST_WAIT
 #define GUEST_WAIT
 
-#define WAIT_QUEUE_LIMIT 20
+#define WAIT_QUEUE_LIMIT 5
 
 struct GuestQueue
 {
@@ -13,8 +13,10 @@ struct GuestQueue
 
 void on_start_guest_wait();
 void on_finish_guest_wait();
-void on_guest_arriving(char *part_name, int party_size);
-void *guest_thread_worker();
+
+void *guest_arrival_worker();
+void *guest_seating_worker();
+
 void print_queue();
 
 #endif
